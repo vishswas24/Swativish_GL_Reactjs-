27ed3839374720359d0b0a7df261535b65da709c
vishswas24 <135657695+vishswas24@users.noreply.github.com>
2024-03-03 23:48:13 +0530

    react js project

---
+++ Frontend Graded Assignment 3 (React JS)/Movies on the Tip/src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet
+} from "react-router-dom";
+
+import Favourites from './components/Favourites';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import MoviesInTheaters from './components/MoviesInTheaters';
+import TopRatedIndian from './components/TopRatedIndian';
+import TopRatedMovies from './components/TopRatedMovies';
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <Router>
+        {/* <Header search={search} /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies-in-theaters" element={<MoviesInTheaters />} />
+          <Route path="/top-rated-indian" element={<TopRatedIndian />} />
+          <Route path="/top-rated-movies" element={<TopRatedMovies />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+      <Outlet />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend Graded Assignment 3 (React JS)/Movies on the Tip/src/components/Header.tsx
@@ -0,0 +1,54 @@
+import { Link, useLocation } from "react-router-dom";
+import { IoSearch } from "react-icons/io5";
+
+interface IParams {
+  search: any
+}
+
+const Header = ({ search }: IParams) => {
+
+  const location = useLocation();
+  console.log(location.pathname)
+
+  return (
+    <div className={'header'}>
+      <ul className="navbar">
+        <li className={location.pathname === "/" ? 'active' : ''}>
+          <Link className="home" to="/">
+            HOME (Movies Coming)
+          </Link>
+        </li>
+        <li className={location.pathname === "/movies-in-theaters" ? 'active' : ''}>
+          <Link className="favorite" to="/movies-in-theaters">
+            Movies in theaters
+          </Link>
+        </li>
+        <li className={location.pathname === "/top-rated-indian" ? 'active' : ''}>
+          <Link className="favorite" to="/top-rated-indian">
+            Top rated Indian
+          </Link>
+        </li>
+        <li className={location.pathname === "/top-rated-movies" ? 'active' : ''}>
+          <Link className="favorite" to="/top-rated-movies">
+            Top rated movies
+          </Link>
+        </li>
+        <li className={location.pathname === "/favourites" ? 'active' : ''}>
+          <Link className="favorite" to="/favourites">
+            favourites
+          </Link>
+        </li>
+      </ul>
+      {location.pathname !== "/favourites" &&
+        <div className="search-container">
+          <input className="search" type="text" onChange={(e) => { search(e.target.value) }}></input>
+          <div className="search-btn">
+            <IoSearch />
+          </div>
+        </div>
+      }
+    </div>
+  );
+};
+
+export default Header;
